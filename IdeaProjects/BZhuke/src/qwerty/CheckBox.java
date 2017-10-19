e3d76b324a21e7c360000aa86bbc48c9252281dd
Zhuldyz Bekbolat <Zhbekbolat@gmail.com>
2017-10-19 12:43:05 +0600

    my sixth commit-"CheckBox.java" озгерис енгизилди.

---
+++ IdeaProjects/BZhuke/src/qwerty/CheckBox.java
@@ -0,0 +1,39 @@
+package qwerty;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CheckBox extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Reading Checkbox data";
+        String docType=
+                "<!doctype html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n"+
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<hl align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                " <li><b>Maths Flag</b>: "
+                + request.getParameter( "maths") + "\n" +
+                "<li><b>Physics Flag</b>: "
+                +request.getParameter( "physics") + "\n" +
+                        "<li><b>Chemistry Flag</b>: "
+                +request.getParameter( "chemistry") + "\n" +
+                "<li><b>History Flag</b>: "
+                +request.getParameter( "history") + "\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+    }
+
